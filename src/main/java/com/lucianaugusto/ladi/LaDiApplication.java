21f9a77db7410d669917f1034fc7ad42075c0701
Lucian Augusto <lucian.augusto.92@gmail.com>
2020-03-06 20:10:01 -0300

    Initial Commit/Project Creation

---
+++ src/main/java/com/lucianaugusto/ladi/LaDiApplication.java
@@ -0,0 +1,13 @@
+package com.lucianaugusto.ladi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucianaugusto/ladi/LaDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianaugusto.ladi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
